05c46224d6a038d9c8da4b2bc6409f3b62316eb3
DianaColm <dcolmenerog@yahoo.com.mx>
2018-04-17 12:09:16 -0500

    subiendo navbar y footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div class="card-footer text-muted text-center">
+          Este es mi Footer
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+      <nav className="navbar-light bg-light text-center">
+        <p className="text-center navbar-brand mb-0 h1" >Trivia</p>
+      </nav>
+
+      </div>
+    );
+  }
+}
+
+export default NavBar;
