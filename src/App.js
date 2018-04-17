5ada35e3ffb08ac192490aa72e3fe6c7088a0bca
biokah <maria.anaisr@gmail.com>
2018-04-17 14:47:44 -0500

    adding answers

--- src/App.js
+++ src/App.js
@@ -9,25 +9,10 @@ import Home from './components/Home'
 
 class App extends Component {
 
-    constructor(props){
-      super(props)
-      this.state = {
-        questions : ''
-      }
-    }
-    componentWillMount(){
-      // console.log(this.state)
-    }
+   
+   
   
-    componentDidMount(){
-      fetch('https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple')
-        .then((response) => {
-          return response.json()
-        })
-        .then((response) => {
-          this.setState({questions: response.results}, function(){console.log(this.state.questions)})
-        })
-    }
+    
 
 
   render() {
@@ -36,14 +21,14 @@ class App extends Component {
 
       <div>
         <NavBar />
-        <p>este es mi contenido</p>
-        <Footer />
+        
       <div className="App">
         <Switch>
           <Route component={Home} path="/" exact />
           <Route component={Questions} path="/questions" />
         </Switch>
       </div>
+      <Footer />
       </div>
     );
   }
